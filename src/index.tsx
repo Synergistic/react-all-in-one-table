c0ad67a3a53616da44adc491ba2545a4e25e6c07
Dan <Synergistic@users.noreply.github.com>
2021-04-14 22:56:29 -0400

    fix dependencies, add showSearch and inputClassName props

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,16 @@ import { AutoSizer, Column, Table } from 'react-virtualized';
 const { useState, useEffect } = React
 
 export type INestedTableProps = {
-  defaultOrder?: "desc"|"asc"|undefined
-  defaultOrderBy?: string
-  data: any[]
-  columns: { dataKey: string, label: string, width: number }[]
+    defaultOrder?: "desc" | "asc" | undefined
+    defaultOrderBy?: string
+    data: any[]
+    columns: { dataKey: string, label: string, width: number }[]
 }
 
-const VirtualizedSortableTable: React.FC<INestedTableProps> = ({ data, columns, defaultOrder='asc', defaultOrderBy='' }) => {
+const VirtualizedSortableTable: React.FC<INestedTableProps> = ({ data, columns, defaultOrder = 'asc', defaultOrderBy = '' }) => {
     const [sortedData, setSortedData] = useState<any[]>([]);
     const [orderBy, setOrderBy] = useState(defaultOrderBy);
-    const [order, setOrder] = useState<"desc"|"asc">(defaultOrder);
+    const [order, setOrder] = useState<"desc" | "asc">(defaultOrder);
 
     useEffect(() => {
         setSortedData(_.orderBy(data, [orderBy], order));
@@ -37,9 +37,9 @@ const VirtualizedSortableTable: React.FC<INestedTableProps> = ({ data, columns, 
                     sortBy={orderBy}
                     sort={handleSort}
                     rowCount={sortedData.length}
-                    rowGetter={({ index }: { index: number}) => sortedData[index]}
+                    rowGetter={({ index }: { index: number }) => sortedData[index]}
                 >
-                    {columns.map(({ dataKey, label, width }: {dataKey: string, label: string, width: number}) => {
+                    {columns.map(({ dataKey, label, width }: { dataKey: string, label: string, width: number }) => {
                         return (
                             <Column
                                 width={width ? width : 200}
@@ -56,56 +56,62 @@ const VirtualizedSortableTable: React.FC<INestedTableProps> = ({ data, columns, 
 }
 
 export type ITableProps = {
-  defaultOrder?: "desc"|"asc"|undefined
-  defaultOrderBy?: string
-  data: any[]
-  columns: { dataKey: string, label: string, width: number }[]
-  ignoreSearchColumns?: string[]
+    defaultOrder?: "desc" | "asc" | undefined
+    defaultOrderBy?: string
+    data: any[]
+    columns: { dataKey: string, label: string, width: number }[]
+    ignoreSearchColumns?: string[]
+    showSearch?: boolean
+    inputClassName?: string
 }
 
-const App: React.FC<ITableProps> = ({ data, columns, ignoreSearchColumns=undefined, defaultOrder='asc', defaultOrderBy=''}) => {
-  const [filteredData, setFilteredData] = useState<any[]>([]);
-  const [searchTerm, setSearchTerm] = useState("")
+const App: React.FC<ITableProps> = ({ data, columns, showSearch = true, ignoreSearchColumns = undefined, defaultOrder = 'asc', defaultOrderBy = '', inputClassName = '' }) => {
+    const [filteredData, setFilteredData] = useState<any[]>([]);
+    const [searchTerm, setSearchTerm] = useState("")
 
-  useEffect(() => {
-      let filtered = _.filter(data, (d: any) => {
-          let match = false;
-          let keys = Object.keys(d);
-          for(let i = 0; i < keys.length; i++){
-              if(match) break;
-              if(ignoreSearchColumns) {
-                  if(ignoreSearchColumns.indexOf(keys[i]) > -1) continue;
-              }
-              if (d[keys[i]]) {
-                  if(d[keys[i]].toString().toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
-                      match = true;
-                  }
-              }
-          }
-          return match;
-      })
-      setFilteredData(filtered)
-  }, [searchTerm, data, ignoreSearchColumns]);
+    useEffect(() => {
+        let filtered = data;
+        if (showSearch) {
+            filtered = _.filter(data, (d: any) => {
+                let match = false;
+                let keys = Object.keys(d);
+                for (let i = 0; i < keys.length; i++) {
+                    if (match) break;
+                    if (ignoreSearchColumns) {
+                        if (ignoreSearchColumns.indexOf(keys[i]) > -1) continue;
+                    }
+                    if (d[keys[i]]) {
+                        if (d[keys[i]].toString().toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+                            match = true;
+                        }
+                    }
+                }
+                return match;
+            })
+        }
+        setFilteredData(filtered)
+    }, [searchTerm, data, ignoreSearchColumns, showSearch]);
 
-  const updateInput = (e: any) => {
-      setSearchTerm(e.target.value);
-  }
+    const updateInput = (e: any) => {
+        setSearchTerm(e.target.value);
+    }
 
-  return (
-      <div style={{ height: '100%' }}>
-          <input placeholder="Search" value={searchTerm} onChange={updateInput} type="text" />
-          <hr />
-          {filteredData.length > 0 &&
-              <VirtualizedSortableTable
-                  data={filteredData}
-                  columns={columns}
-                  defaultOrderBy={defaultOrderBy}
-                  defaultOrder={defaultOrder}
-              />
-          }
-      </div>
+    return (
+        <div style={{ height: '100%' }}>
+            {showSearch &&
+                <input placeholder="Search" value={searchTerm} onChange={updateInput} type="text" className={inputClassName} style={{ marginBottom: '8px'}} />
+            }
+            {filteredData.length > 0 &&
+                <VirtualizedSortableTable
+                    data={filteredData}
+                    columns={columns}
+                    defaultOrderBy={defaultOrderBy}
+                    defaultOrder={defaultOrder}
+                />
+            }
+        </div>
 
-  );
+    );
 }
 
 export default App
